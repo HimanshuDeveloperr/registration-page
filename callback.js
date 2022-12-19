99a2028f08e180a175cb1e63cb05bfcc9e2bd41b
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2022-12-20 01:11:36 +0530

    printing on webpage the created post with time

--- callback.js
+++ callback.js
@@ -1,60 +1,29 @@
-const posts=[
-    { title:'post 1',body:'this is post 1',createdAt:new Date().getTime()},
-    { title:'post 2',body:'this is post 2',createdAt:new Date().getTime()}
+//creating a post
+
+const post=[
+    {name:'himanshu',work:'frontend-developer',createdpost:new Date().getTime()},
+    {name:'suman',work:'professional footballer',createdpost:new Date().getTime()}
 
 ]
 
-let intervalId=0;
-function printPostsInDOCUMENT() {
-    clearInterval(intervalId)
+//function to show post on our webpage
 
-    intervalId=setInterval(() => {
-
-      setTimeout(() => {
-  
-          let output="";
-          
-           posts.forEach((post) => {
-                                                              //currentTime-createdTIME/1000
-               output=output+`<li>${post.title} - last updated ${(new Date().getTime()-post.createdAt) / 1000} seconds ago</li>`
-                                                                 //  new.Date().getTime()=current time
-           });
-           document.body.innerHTML=output;
-  
-  
-  }, 1000); 
-    
-  }, 1000);
-
-    
-}
-
-// printPostsInDOCUMENT();
-
-function createPost(newpost,callback){
-    setTimeout(() => {
-                          // ading createdat through spread operator in our object when we r creating a new post
-             posts.push({...newpost,createdAt:new Date().getTime()});
-        
-             callback()
-    }
-    ,2000);
-}
-
-createPost({title:'post 3',body:'this is new post'},printPostsInDOCUMENT)
-
-
-function create4Post(p,callback) {
+function getpost(){
 
     setTimeout(() => {
 
-        posts.push({...p,createdAt:new Date().getTime()})
-        callback();
+       
 
-      
+            let output="";
+            post.forEach((obj)=>{
+              let li=`<li>${obj.name} ${obj.work} updated on ${(new Date().getTime()-obj.createdpost)/1000} seconds ago`
+                output=output+ li
+            })
+            document.body.innerHTML=output;
+            
+     
         
-    }, 6000);
-    
+    }, 2000);
 }
 
-create4Post({title:'post 4',body:'this is post 4'},printPostsInDOCUMENT)
+getpost()
